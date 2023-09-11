99a0304801a0eac7b601b607f8f9a1efa8344f2d
FilinSky <daniel.filinski@gmail.com>
2023-09-10 17:55:37 -0700

    tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "./redux/store";
+import { getData} from "./redux/storeSlice";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ModalWindow from "./components/ModalWindow";
+
+import RecursiveComponent from "./components/RecursiveComponent";
+import Status from "./components/Status";
+import CollapseButtons from "./components/CollapseButtons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const dispatch = useDispatch()
+    const {data, status} = useSelector((state: RootState) => {
+        return state.store
+    })
+
+    useEffect(() => {//@ts-ignore
+        if (status === 'firstStart' || status === 'added' ) {
+            //@ts-ignore
+            dispatch(getData())
+        }
+    }, [dispatch, status])
+
+    return (
+        <div className={'main'}>
+            <ModalWindow/>
+            <Status/>
+            <CollapseButtons/>
+            <RecursiveComponent data={[data]} />
+        </div>
+    )
 }
 
 export default App;
+
